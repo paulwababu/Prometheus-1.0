f92529e0de5b44867a35ae1a56b3dae4041c1605
paulwababu <44939805+paulwababu@users.noreply.github.com>
2021-02-07 02:00:47 +0300

    Update mpesa_credentials.py

--- bot_app/mpesa_credentials.py
+++ bot_app/mpesa_credentials.py
@@ -5,8 +5,8 @@ from datetime import datetime
 import base64
 
 class MpesaC2bCredential:
-    consumer_key = 'qaB31tWrbH4psywuGIfGnvekxMganto4'
-    consumer_Secret = 'YlH4kc0RqJrkmQJy'
+    consumer_key = '#'
+    consumer_Secret = '#'
     api_URL = 'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
 
 class MpesaAccessToken:
@@ -17,9 +17,9 @@ class MpesaAccessToken:
 
 class LipanaMpesaPpassword:
     lipa_time = datetime.now().strftime('%Y%m%d%H%M%S')
-    Business_short_code = "7238823"
-    Test_c2b_shortcode = "5297073"
-    passkey = '77107c0f097df2f9475d5b8a676dc2bd331436e8f0dc82ae879ce3a583730935'
+    Business_short_code = "#"
+    Test_c2b_shortcode = "#"
+    passkey = '#'
     data_to_encode = Business_short_code + passkey + lipa_time
     online_password = base64.b64encode(data_to_encode.encode())
     decode_password = online_password.decode('utf-8')
